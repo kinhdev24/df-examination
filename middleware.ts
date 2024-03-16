bfa5d3e1887f3947a839bfb1aa4e1cb65e73625a
kinhdev24 <kinhdev24@gmail.com>
2024-03-17 01:28:56 +0700

    fix redirect

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,32 @@ import createIntlMiddleware from "next-intl/middleware"
 import { NextRequest, NextResponse } from "next/server"
 import { locales } from "./shared/config"
 
-export default async function middleware(
-  request: NextRequest,
-  res: NextResponse
-) {
-  const requestForNextAuth = {
-    headers: {
-      cookie: request.headers.get("cookie"),
-    },
-  } as any
-
+export default async function middleware(request: NextRequest) {
   // Step 1: Use the incoming request (example)
   const defaultLocale = request.headers.get("x-your-custom-locale") || "en"
 
-  const [, locale, ...segments] = request.nextUrl.pathname.split("/")
+  const pathName = request.nextUrl.pathname
+  const url = request.url
+
+  const [, locale, ...segments] = pathName.split("/")
 
   const token = await getToken({ req: request, raw: true })
+
+  if (!locale) {
+    if (token) {
+      request.nextUrl.pathname = `/en/dashboard`
+      return NextResponse.redirect(request.nextUrl.toString())
+    } else {
+      request.nextUrl.pathname = `/en/login`
+      return NextResponse.redirect(request.nextUrl.toString())
+    }
+  }
+
+  if (!!token && !segments.length) {
+    request.nextUrl.pathname = `/${locale}/dashboard`
+
+    return NextResponse.redirect(request.nextUrl.toString())
+  }
 
   if (!token && !segments.includes("login")) {
     request.nextUrl.pathname = `/${locale}/login`
